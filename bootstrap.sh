b14927c72315f9c2ec8c0f5475075acbf6b75994
Rogier Krieger <rkrieger@users.noreply.github.com>
2022-02-12 23:59:59 +0100

    Prepare bootstrap shell script for Xcode CLI, Brew and Ansible.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,1 +1,12 @@
 #!/bin/sh
+
+# Xcode CLI utilities
+xcode-select --install
+
+# Homebrew
+curl -fsSL -o brew_install.sh https://raw.githubusercontent.com/Homebrew/install/master/install.sh
+chmod +x ./brew_install.sh
+/bin/bash -c ./brew_install.sh
+
+# Ansible
+brew install ansible
