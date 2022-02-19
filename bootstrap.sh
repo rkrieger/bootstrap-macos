2cb0e7b2a8cc3017dc943e657871f44d9a29832d
Rogier Krieger <rkrieger@users.noreply.github.com>
2022-02-19 21:42:40 +0100

    Ensure homebrew is part of $PATH.

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,5 +8,27 @@ curl -fsSL -o brew_install.sh https://raw.githubusercontent.com/Homebrew/install
 chmod +x ./brew_install.sh
 /bin/bash -c ./brew_install.sh
 
+# Ensure Homebrew is in ${PATH}
+_arch=`uname -m`
+_shell=`basename $SHELL`
+if [ X"${_shell}" == X"zsh" ]; then
+    _profile=~/.zprofile
+elif [ X"${_shell}" == X"bash" ]; then
+    _profile=~/.bash_profile
+else
+    _profile=~/.profile
+fi
+if [ X"${_arch}" == X"arm64" ]; then
+    _brewdir=/opt/homebrew
+else
+    _brewdir=/usr/local
+fi
+_brew="${_brewdir}/bin/brew shellenv"
+_eval1='eval "$('
+_eval2=')"'
+echo "${_eval1}${_brew}${_eval2}" >> ${_profile}
+eval "${_eval1}${_brew}${_eval2}"
+
+
 # Ansible
 brew install ansible
